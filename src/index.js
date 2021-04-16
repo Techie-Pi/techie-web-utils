150cf5671fcb0b849ff6eaac9c0be1ab1acc202c
TechiePi <techiepi@pm.me>
2021-04-16 12:53:42 +0200

    feat: create state system
    w/tests

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const ui =  require("./modules/ui");
 const logger = require("./modules/logger");
 const i18n = require("./modules/i18n");
 const events = require("./modules/events");
+const state = require("./modules/state");
 
-module.exports = { ui, logger, i18n, events };
+module.exports = { ui, logger, i18n, events, state };
