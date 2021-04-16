9ca1570ca7f5eee41a67791f6e838c246e9fb42e
TechiePi <techiepi@pm.me>
2021-04-16 12:41:57 +0200

    feat: create event system
    w/tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const ui =  require("./modules/ui");
 const logger = require("./modules/logger");
 const i18n = require("./modules/i18n");
+const events = require("./modules/events");
 
-module.exports = { ui, logger, i18n };
+module.exports = { ui, logger, i18n, events };
